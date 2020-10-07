c4f59421bc95184e01fb687a30d778122406f3c4
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-10-07 17:12:15 -0300

    Project created

---
+++ backend/src/test/java/com/rafael/dsdCatalog/DsdCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.dsdCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
