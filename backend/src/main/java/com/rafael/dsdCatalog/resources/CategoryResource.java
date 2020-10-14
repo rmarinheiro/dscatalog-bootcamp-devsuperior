045c0d7a360a95dcae5103e975bfbcc96a5169c0
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-10-13 23:47:05 -0300

    Metodo de inserção de uma nova category

--- backend/src/main/java/com/rafael/dsdCatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/rafael/dsdCatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.rafael.dsdcatalog.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafael.dsdcatalog.dto.CategoryDTO;
 import com.rafael.dsdcatalog.entities.Category;
@@ -29,6 +33,15 @@ public class CategoryResource {
 //		list.add( new Category(1L, "Books"));
 //		list.add( new Category(2L, "Electronics"));
 		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = categoryService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 		
 	}
 	
